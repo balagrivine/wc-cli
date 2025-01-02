ef8c5ed5159c634a739b562059dbe8ecadabeae3
balagrivine <balagrivine@gmail.com>
2025-01-02 04:33:06 -0800

    feat: add stdin stream processing for byte and word count

--- cmd/byte.go
+++ cmd/byte.go
@@ -3,12 +3,24 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"io"
 
 	"github.com/spf13/cobra"
 )
 
 func CountBytes(cmd *cobra.Command, args []string) {
 
+	// Read from stdin stream if no file is provided
+	if len(args) == 0 {
+		bytesRead, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(cmd.OutOrStdout(), "%v", err)
+		}
+
+		fmt.Fprintf(cmd.OutOrStdout(), "%d\n", len(bytesRead))
+	}
+
+	// Else read from file(s) provided
 	for _, val := range args {
 
 		data, err := os.ReadFile(val)
--- cmd/word.go
+++ cmd/word.go
@@ -11,6 +11,22 @@ import (
 
 func CountWords(cmd *cobra.Command, args []string) {
 
+	// Scan from stdin if no file is provided
+	if len(args) == 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		wordCount := 0
+
+		for scanner.Scan() {
+			wordCount += len(strings.Fields(scanner.Text()))
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(cmd.OutOrStdout(), "%v\n", err)
+		}
+
+		fmt.Fprintf(cmd.OutOrStdout(), "%d\n", wordCount)
+	}
+	
+	// Else scan from the file(s) provided
 	for _, val := range args {
 		file, err := os.Open(val)
 		if err != nil {
